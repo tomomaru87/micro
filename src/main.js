e3f5bcef00c993ce6578e8dbd328b784e978936d
tomomaru87 <tikksyou1129@gmail.com>
2022-03-06 17:14:34 +0900

    noindexの設定を追加した

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router/index'
+import VueHead from 'vue-head'
 
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(router,VueHead).mount('#app')
